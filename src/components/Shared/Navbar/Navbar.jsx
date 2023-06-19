1d1eb6660338d004263d137a656e638eaad1ff15
Ridoy Sharif <developersridoy@gmail.com>
2023-06-19 13:26:19 +0600

    install complete and base setup complete

---
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div className='py-4 border-b-[1px]'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
