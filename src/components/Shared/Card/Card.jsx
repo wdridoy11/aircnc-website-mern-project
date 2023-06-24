497d583ff03e95e8a24d8dc6cbf3a31134ddec9b
Ridoy Sharif <developersridoy@gmail.com>
2023-06-24 19:51:11 +0600

    rooms image and other info added successful

--- src/components/Shared/Card/Card.jsx
+++ src/components/Shared/Card/Card.jsx
@@ -1,22 +1,21 @@
 import HeartButton from "../Button/HeartButton"
 
 const Card = ({room}) => {
-console.log(room)
+  const {image, category, location, price, dateRange}= room;
   return (
     <div className='col-span-1 cursor-pointer group'>
       <div className='flex flex-col gap-2 w-full'>
         <div className='aspect-square w-full relative overflow-hidden rounded-xl'>
-          <img className='object-cover h-full w-full group-hover:scale-110 transition'
-            src='https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg' alt='Room'/>
+          <img className='object-cover h-full w-full group-hover:scale-110 transition' src={image} alt='Room'/>
           <div className=' absolute top-3 right-3'>
             <HeartButton></HeartButton>
           </div>
         </div>
-        <div className='font-semibold text-lg'>Sidemen, Indonesia</div>
-        <div className='font-light text-neutral-500'>5 nights . June 19 - 26</div>
+        <div className='font-semibold text-lg'>{location}</div>
+        <div className='font-light text-neutral-500'>{dateRange}</div>
         <div className='flex flex-row items-center gap-1'>
-          <div className='font-semibold'>$ 200</div>
-          <div className='font-light'>night</div>
+          <div className='font-semibold'>$ {price}</div>
+          <div className='font-light'>{category}</div>
         </div>
       </div>
     </div>
