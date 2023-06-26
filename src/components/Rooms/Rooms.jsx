d7ee3f0750f2b518add7492bc0a188a9d9383af6
Ridoy Sharif <developersridoy@gmail.com>
2023-06-26 11:59:03 +0600

    no rooms available heading added

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -3,6 +3,7 @@ import Card from './Card';
 import Container from '../Shared/Container';
 import Loader from '../Shared/Loader/Loader';
 import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 
 const Rooms = () => {
     const [rooms,setRooms]= useState([]);
@@ -31,10 +32,19 @@ const Rooms = () => {
   return (
     <div>
         <Container>
-            {loading && <Loader></Loader>}
-            <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5'>
-                {rooms.map((room,index)=><Card key={index} room={room}></Card>)}
-            </div>
+            {rooms && rooms.length >0 ?<>
+                {loading && <Loader></Loader>}
+                <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5'>
+                    {rooms.map((room,index)=><Card key={index} room={room}></Card>)}
+                </div>
+            </>:
+            <div className='mt-5'>
+                <Heading 
+                    title="No Rooms Available In This Category!" 
+                    subtitle="Please Select Other Categories" 
+                    center={true}>
+                </Heading>
+            </div>}
         </Container>
     </div>
   )
